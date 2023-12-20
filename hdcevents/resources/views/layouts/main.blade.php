71f81319f3257b879b1eb690ab8a8f37a5f835e8
Hertzy <helter.xavier@hotmail.com>
2023-12-20 14:27:24 -0300

    20122023

---
+++ hdcevents/resources/views/layouts/main.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>@yield('title')</title>
+
+    <!-- Fonts -->
+
+    {{-- BOOTSTRAP --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    {{-- CSS --}}
+    <link rel="stylesheet" href="/css/styles.css">
+    {{-- JS - SCRIPT --}}
+    <script src="/js/scripts.js"></script>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="collapse navbar-collapse" id="navbar">
+                <a href="/" class="navbar-brand">
+                    <h2>Logo</h2>
+                </a>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="/" class="nav-link">Eventos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/" class="nav-link">Criar Eventos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/" class="nav-link">Entrar</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/" class="nav-link">Cadastrar</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    @yield('content')
+    
+    <footer>
+        <p>HDC Events &copy; 2023</p>
+    </footer>
+</body>
+
+</html>
--- hdcevents/routes/web.php
+++ hdcevents/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/product/{id}', function ($id) {
+    return view('product', ['id' => $id]);
+});
