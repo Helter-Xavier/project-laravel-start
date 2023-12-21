effc09bfd2bae39b401425145ec74fae3aedd4f8
Hertzy <helter.xavier@hotmail.com>
2023-12-21 14:59:44 -0300

    21122023

--- hdcevents/resources/views/welcome.blade.php
+++ hdcevents/resources/views/welcome.blade.php
@@ -8,6 +8,11 @@
     <p>{{ $event->title }} -- {{ $event->description }}</p>
 @endforeach
 
-
+<div id="search-container" class="col-md-12">
+    <h1>Busque um evento</h1>
+    <form action="">
+        <input type="text" id="search" name="search" class="form-control">
+    </form>
+</div>
 
 @endsection
